bac6c8499ca898021d77718831a254e9cf824f14
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-03-08 16:01:55 +0100

    Create cr_environment_install.sh

---
+++ cr_environment_install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "Installing all dependencies for runc, containerd, docker and criu"
+
+# -- apt packages
+apt-get update 
+apt-get install wget git
+
+# -- runc
+# first we must install the latest golang 
+echo "Installing latest golang"
+wget -O /opt/go https://go.dev/dl/go1.17.8.linux-amd64.tar.gz
+cd /opt/go
+tar -C /usr/local -xzf go1.17.8.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+echo "-- installed version --" # todo make this an aassert
+go version
+
+git clone https://github.com/opencontainers/runc.git /opt/runc
+cd /opt/runc 
+make && make install 
+
+# -- containerd 
+wget -0 /opt/containerd https://github.com/containerd/containerd/releases/download/v1.6.1/cri-containerd-cni-1.6.1-linux-amd64.tar.gz
+cd /opt/containerd
+sudo tar --no-overwrite-dir -C / -xzf cri-containerd-cni-1.6.1-linux-amd64.tar.gz
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
+echo "-- installed version --" # todo make this an assert
+containerd --version
+
+# -- docker
+apt-get install ca-certificates curl gnupg lsb-release
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update 
+apt-get install docker-ce docker-ce-cli
+echo "-- installed version --" # todo make this an assert
+docker version 
+
+# -- criu 
+apt-get install build-essential libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python-protobuf pkg-config python-ipaddress libbsd-dev iproute2 nftables libcap-dev libnet1-dev libnl-3-dev libaio-dev python3-future asciidoc xmlto vim python3-distutils libnftables-dev
+--no-install-recommends -y 
+git clone https://github.com/checkpoint-restore/criu.git /opt/criu
+cd /opt/criu 
+make clean 
+make 
+make install 
+echo "-- installed version --"
+criu version 
+criu check 
+
+echo "INSTALL DONE"
+echo "Reboot to check if it worked"
