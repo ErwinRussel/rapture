00d6a7c293953cddf5ef55cffa4cf10625034a86
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-03-22 16:36:13 +0100

    Working migration script
    This script describes a working migration of containers. Missing arg variables are however the user and the location of the SSH keys

--- scheduler/migrate.sh
+++ scheduler/migrate.sh
@@ -1,36 +1,60 @@
 
-while getopts u:s:t:c: flag
+# For migration to run make sure that you have run # sudo apt install nfs-kernel-server nfs-common
+while getopts s:t:c: flag
 do
     case "${flag}" in
+        s) SRC_IP=${OPTARG};;
         t) TRG_IP=${OPTARG};;
         c) SRC_CID=${OPTARG};;
     esac
 done
 
-echo $TRG_IP
-echo $SRC_CID
-
+echo "Preparing migration for container: ${SRC_CID} to host ${TRG_IP}"
 # -- PRE-MIGRATE ---
+echo "--------------------\n---- PRE-MIGRATE ---\n--------------------"
 # Get Source image from docker
-SRC_IMG=sudo docker inspect ${SRC_CID} | grep -oP '(?<='sha256:')(.*)(?='\"')'
+SRC_IMG=$(docker inspect "$SRC_CID" | grep -oP '(?<='sha256:')(.*)(?='\"')')
+echo "- Source Image:"
+echo $SRC_IMG
 
 # The source prepares a container on the target
-TRG_CID=ssh ${USER}@${TRG_IP} "sudo docker create ${SRC_IMG}"
+TRG_CID=$(ssh -i ${SSH_KEY} ${USR}@${TRG_IP} "sudo docker create ${SRC_IMG}")
+echo "- Target container created, target container:"
+echo $TRG_CID
 
 # The source opens the nfs file directory for the target in the source checkpoint folder and the target container checkpoint folder
-sudo mkdir /opt/checkpoints/${SRC_CID}
-sudo chown nobody:nogroup /opt/checkpoints/${SRC_CID}
-sudo chmod 777 /opt/checkpoints/${SRC_CID}//opt/checkpoints/${SRC_CID}
-echo "Folder created"
-ssh ${USER}@${TRG_IP} "sudo mount -t nfs -o vers=4 -o resvport ${SRC_IP}:/opt/checkpoints/${SRC_CID}  /var/lib/docker/containers/${TRG_CID}/checkpoints" 
-echo "Container and NFS connection set up"
+echo "- Making directory.."
+mkdir /opt/checkpoints/${SRC_CID}
+echo "- Adding permissions to directory.."
+chown nobody:nogroup /opt/checkpoints/${SRC_CID}
+echo "- Changing permissions to directory.."
+chmod -R 777 /opt/checkpoints/${SRC_CID}
+echo "- Folder created"
+echo "/opt/checkpoints/${SRC_CID} *(rw,sync,no_subtree_check)" >> /etc/exports
+echo "- Added to exports"
+echo "- Restarting NFS"
+sudo systemctl restart nfs-kernel-server
+echo "- Current shared directories by NFS:"
+sudo exportfs
+#echo "Press any key to connect nfs on target container"
+#read y
 
-echo "Everything set-up, press key to checkpoint-migrate-restor"
+ssh -i ${SSH_KEY} ${USR}@${TRG_IP} "sudo mount -t nfs -o vers=4 -o resvport ${SRC_IP}:/opt/checkpoints/${SRC_CID}  /var/lib/docker/containers/${TRG_CID}/checkpoints"
+echo "- Container and NFS connection set up"
+
+echo "\n\n- Everything set-up, press any key to checkpoint-migrate-restore"
 read r
-# -- MIGRATE -- 
-# Checkpoint container 
-sudo docker checkpoint create ${SRC_CID} cr1
-ssh ${USER}@${TRG_IP} "sudo docker restore --checkpoint=cr1 ${SRC_IMG}"
 
-# sudo apt install nfs-kernel-server nfs-common
-# sudo docker inspect $SRC_ID | grep -o 'sha256:\(.*\)"'
+# -- MIGRATE --
+# start_time=$(date +%s.%3N)
+echo "--------------------\n------ MIGRATE -----\n--------------------"
+# Checkpoint container 
+sudo docker checkpoint create ${SRC_CID} --checkpoint-dir /opt/checkpoints/${SRC_CID} cr1
+chmod -R 777 /opt/checkpoints/${SRC_CID}
+ssh -i ${SSH_KEY} ${USR}@${TRG_IP} "sudo docker start --checkpoint=cr1 ${TRG_CID}"
+# end_time=$(date +%s.%3N)
+# elapsed=$($end_time - $start_time)
+# echo "- Migration succesful in: ${elapsed} seconds."
+echo "- Logs:"
+ssh -i ${SSH_KEY} ${USR}@${TRG_IP} "sudo docker logs ${TRG_CID}"
+
