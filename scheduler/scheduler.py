4adbbb159a97be63dab6bbc43b65aaa824544cf2
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-05 14:21:24 +0200

    add dynamic ports

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -89,10 +89,12 @@ class Scheduler:
 
         node = self.get_node_strategy()
         networks = ['rapture-monitoring_monitoring']
+        # uuid = name + shortuuid.uuid()
+        cur_id = self.get_serv_id()
+        uuid = "rapture_game_" + str(cur_id)
+        dynamic_port = 8000 + cur_id
         endpoint_spec = docker.types.EndpointSpec(ports={8000:8000})
         cont_resources = docker.types.Resources(cpu_reservation=cpu_req, cpu_limit=cpu_req, mem_reservation=mem_req, mem_limit=mem_req)
-        # uuid = name + shortuuid.uuid()
-        uuid = "rapture_game_" + str(self.get_serv_id())
         if (self.strategy != StrategyEnum.spread):
             if node is None:
                 print("No node to schedule")
