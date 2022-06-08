85eb65a6650d961c10356e15797ddfa2bb29b148
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-08 21:14:11 +0200

    now we got it hopefully

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -131,7 +131,9 @@ class Scheduler:
     def get_hostname(self, service):
         for node in self.strategies.node_resource_dict.keys():
             print(node)
-            print([s.attrs['Spec']['Name'] for s in self.client.services.list() for _ in s.tasks({'node': node, 'desired-state': 'Running'})])
+            print(service.tasks({'node': node, 'desired-state': 'Running'}))
+            print("Other output")
+            print([s for s in self.client.services.list() for _ in s.tasks({'node': node, 'desired-state': 'Running'})])
         # return container.attrs
 
     def deschedule_game(self):
