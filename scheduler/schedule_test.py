f0a63423b18d07e86bb2bb0894658820bff73316
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-16 12:28:56 +0200

    Update schedule_test.py

--- scheduler/schedule_test.py
+++ scheduler/schedule_test.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     scheduler = Scheduler(StrategyEnum.spread)
 
     #todo: load game container db
-    game_name = "Viking_Village_Linux"
+    game_name = "atlasgears"
     game_spec = scheduler.get_game_spec(game_name)
 
     scheduler.schedule_game(game_spec)
