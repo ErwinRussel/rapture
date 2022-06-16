e7196e27dd97cd00e6b5944c3677edec6fd6c675
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-16 12:29:52 +0200

    Update schedule_test.py

--- scheduler/schedule_test.py
+++ scheduler/schedule_test.py
@@ -4,12 +4,13 @@ import json
 
 if __name__ == '__main__':
     # todo: set this with argv
-    scheduler = Scheduler(StrategyEnum.spread)
+    scheduler = Scheduler(StrategyEnum.rapture)
 
     #todo: load game container db
     game_name = "atlasgears"
     game_spec = scheduler.get_game_spec(game_name)
 
     scheduler.schedule_game(game_spec)
+    time.sleep(1000)
     scheduler.deschedule_game()
 
