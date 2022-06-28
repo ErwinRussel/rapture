b9523177e47ed45265d619cee9fdad3f98b51be7
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-28 12:23:25 +0200

    Update migrater

--- scheduler/migrater.py
+++ scheduler/migrater.py
@@ -1,5 +1,6 @@
 import shortuuid
 import docker
+import time
 
 
 class Migrater:
@@ -81,52 +82,73 @@ class Migrater:
             game_spec = self.scheduler.get_game_spec("Viking_Village_Linux")
             self.scheduler.schedule_game(game_spec)
 
-    # def schedulerestore(self, game_spec):
-    #     # print("Restoring service: {}".format(service.name))
-    #     name = game_spec['name']
-    #     print("Restoring service: {}".format(name))
-    #     image = game_spec['image']
-    #     envir = game_spec['env'].append("ATL_RESTORE=1")
-    #     cpu_req = game_spec['requirements']['cpu_req']
-    #     mem_req = game_spec['requirements']['mem_req']
-    #     vmem_req = game_spec['requirements']['vmem_req']
-    #     frametime_req = game_spec['requirements']['frametime_req']
-    #     command = ['bash -c "run.sh"']
-    #     mounts = game_spec['mounts']
-    #
-    #     node = self.scheduler.get_node_strategy(cpu_req, mem_req, vmem_req, frametime_req)
-    #     networks = ['rapture_monitoring']
-    #     uuid = name + shortuuid.uuid()
-    #     endpoint_spec = docker.types.EndpointSpec()
-    #     gpu_constr_str = "node.labels.gpu-node == 1"
-    #     cont_resources = docker.types.Resources(cpu_reservation=cpu_req, cpu_limit=cpu_req, mem_reservation=mem_req,
-    #                                             mem_limit=mem_req)
-    #     if node is None:
-    #         print("No node to schedule")
-    #         return
-    #
-    #     print("Scheduling {} on node {}".format(uuid, str(node)))
-    #     self.scheduler.add_limitations(node)
-    #     node_constr_str = "node.hostname=={}".format(node)
-    #     self.scheduler.client.services.create(image=image, command=command, name=uuid, env=envir, resources=cont_resources,
-    #                                 mounts=mounts, constraints=[gpu_constr_str, node_constr_str],
-    #                                 labels={"GAME": "1"}, endpoint_spec=endpoint_spec, networks=networks)
+    def schedulerestore(self, game_spec):
+        # print("Restoring service: {}".format(service.name))
+        name = game_spec['name']
+        print("Restoring service: {}".format(name))
+        image = game_spec['image']
+        envir = game_spec['env'].append("ATL_RESTORE=1")
+        cpu_req = game_spec['requirements']['cpu_req']
+        mem_req = game_spec['requirements']['mem_req']
+        vmem_req = game_spec['requirements']['vmem_req']
+        frametime_req = game_spec['requirements']['frametime_req']
+        command = ['bash -c "run.sh"']
+        mounts = game_spec['mounts']
+
+        node = self.scheduler.get_node_strategy(cpu_req, mem_req, vmem_req, frametime_req)
+        networks = ['rapture_monitoring']
+        uuid = name + shortuuid.uuid()
+        endpoint_spec = docker.types.EndpointSpec()
+        gpu_constr_str = "node.labels.gpu-node == 1"
+        cont_resources = docker.types.Resources(cpu_reservation=cpu_req, cpu_limit=cpu_req, mem_reservation=mem_req,
+                                                mem_limit=mem_req)
+        if node is None:
+            print("No node to schedule")
+            return
+
+        print("Scheduling {} on node {}".format(uuid, str(node)))
+        self.scheduler.add_limitations(node)
+        node_constr_str = "node.hostname=={}".format(node)
+        self.scheduler.client.services.create(image=image, command=command, name=uuid, env=envir, resources=cont_resources,
+                                    mounts=mounts, constraints=[gpu_constr_str, node_constr_str],
+                                    labels={"GAME": "1"}, endpoint_spec=endpoint_spec, networks=networks)
 
 
 
     def deschedulecheckpoint(self, desch_service):
-        # todo: add exec command to checkpoint and write to file
-        # print("Checkpointing service: {}".format(desch_service.name))
-        # container = None
-        # command = ['sh checkpoint.sh']
-        # container.exec_run(command,detach=True)
+        print("Checkpointing service: {}".format(desch_service.name))
+        host_name = self.scheduler.get_hostname_service(desch_service)
+
+        # todo: add container
+        container = None
+        command = ['sh checkpoint.sh']
+        self.exec_command(command, container, host_name)
 
         # todo: remove
         print("Evacuating service: {}".format(desch_service.name))
-        host_name = self.scheduler.get_hostname_service(desch_service)
         self.scheduler.remove_limitations(host_name)
         # if DEBUG:
         #     self.print_resources()
         desch_service.remove()
+
+    def exec_command(self, command, container, node):
+        name = "raptureCC"
+        uuid = name + shortuuid.uuid()
+        image = "erwinrussel/rapturecc:latest"
+        mounts = ['/var/run/docker.sock:/var/run/docker.sock:rw']
+        networks = ['rapture_monitoring']
+
+        print("{}: Running {} on container {}".format(node, command, container))
+
+        node_constr_str = "node.hostname=={}".format(node)
+        restart_policy = docker.types.RestartPolicy(condition="none")
+        ephemeral_service = self.scheduler.client.services.create(image=image, command=command, name=uuid,
+                                                   restart_policy=restart_policy, constraints=[node_constr_str],
+                                                   mounts=mounts, labels={"CC": "1"}, networks=networks)
+        print("Executed")
+        time.sleep(1)
+        print("Removing Service")
+        ephemeral_service.remove()
+        print("Service removed")
 
 
