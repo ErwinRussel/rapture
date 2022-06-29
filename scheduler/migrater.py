30a248972fb6e9e2ede2f46e6f0d1780918fd1cb
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2022-06-29 11:16:37 +0200

    Update migrater.py

--- scheduler/migrater.py
+++ scheduler/migrater.py
@@ -89,7 +89,9 @@ class Migrater:
         print("Restoring service: {}".format(name))
         # image = game_spec['image']
         image = "erwinrussel/atlasgears:nvglrestore" # todo: for testing hardcode a restore image
-        envir = game_spec['env'].append("ATL_RESTORE=1")
+        envir = game_spec['env']
+        envir.append("ATL_RESTORE=1")
+        print(envir)
         cpu_req = game_spec['requirements']['cpu_req']
         mem_req = game_spec['requirements']['mem_req']
         vmem_req = game_spec['requirements']['vmem_req']
